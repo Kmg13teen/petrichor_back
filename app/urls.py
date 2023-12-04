27ad14c14cfef6e4ea360ae650f9f61dc9fa0b63
Kshitij.G <112101025@smail.iitpkd.ac.in>
2023-12-05 03:12:34 +0530

    first

---
+++ app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = "home"),
+    path('api/login/',views.user_login,name="login"),
+    path('api/register/', views.signup, name="signup"),
+    path('api/logout/',views.user_logout,name="logout"),
+    path('api/mt/',views.mailtest ,name="mailtest"),
+    path('api/admin/',views.admin_data ,name="admin"),
+    path('api/events/apply',views.apply_event ,name="applyEvent"),
+    path('api/events/',views.getEventUsers ,name="event"),
+    path('api/events/verify',views.verifyCA,name="verifyCA"),
+    path('api/verifyTR',views.verifyTR,name="verifyTR"),
+    path('api/send_grievance',views.send_grievance,name="send_grievance"),
+    path('api/events/unconfirmed',views.getUnconfirmed ,name="unconfirmed"),
+    path('activate/<uidb64>/<token>', views.activate, name="activate"),
+]
